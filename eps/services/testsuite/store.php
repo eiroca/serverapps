157047c179256a2553a162b902737c1fbfb94a27
umibozu <umibozu@f67a188b-4849-0410-a290-bb9dae77c786>
2008-04-15 20:53:30 +0000

    Update to be aligned with TestSuite 1.0.3

--- eps/services/testsuite/store.php
+++ eps/services/testsuite/store.php
@@ -1,12 +1,14 @@
 <?php
-  global $CONTEXT;
+  global $CONTEXT, $CONFIG;
   require_once ("../../lib/eps/eps.inc");
-  initContext();
   $data = file_get_contents("php://input");
-  $rr = $CONTEXT->touchCounter("req");
-  $fname = $CONFIG["path_log"] . $rr . ".txt";
+  initContext();
+  $rr = trim($CONTEXT->touchCounter("req"));
+  $fname = $CONFIG["path_log"] . (int)$rr . ".txt";
   $f = fopen($fname, "w+");
-  fputs($f, $data);
+  foreach($_REQUEST as $name => $value) {
+    fwrite($f, $name . "=" . $value . "\n");
+  }
   fclose($f);
 ?>
 OK
