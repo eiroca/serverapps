92ec780f76ca153f669b02a62b82d48313078238
umibozu <umibozu@f67a188b-4849-0410-a290-bb9dae77c786>
2008-09-24 16:56:07 +0000

    WURFL2PHP 1.0.0

---
+++ tags/WURF2PHP_1.0.0/WURFL2PHP/src/WURFL2PHP.dpr
@@ -0,0 +1,38 @@
+(****************************************************************************)
+(*  Copyright (C) 1996-2008 eIrOcA Enrico Croce & Simona Burzio             *)
+(*                                                                          *)
+(*  This program is free software: you can redistribute it and/or modify    *)
+(*  it under the terms of the GNU General Public License as published by    *)
+(*  the Free Software Foundation, either version 3 of the License, or       *)
+(*  (at your option) any later version.                                     *)
+(*                                                                          *)
+(*  This program is distributed in the hope that it will be useful,         *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of          *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *)
+(*  GNU General Public License for more details.                            *)
+(*                                                                          *)
+(*  You should have received a copy of the GNU General Public License       *)
+(*  along with this program.  If not, see <http://www.gnu.org/licenses/>.   *)
+(*                                                                          *)
+(*--------------------------------------------------------------------------*)
+(*                                                                          *)
+(* Author :  Enrico Croce                                                   *)
+(*                                                                          *)
+(****************************************************************************)
+program WURFL2PHP;
+
+uses
+  Forms,
+  eHashList in '..\..\library\mirror\eHashList.pas',
+  FAbout in '..\..\library\mirror\FAbout.pas' {fmAbout},
+  FMain in 'gui\FMain.pas' {fmMain},
+  uWURFL in '..\..\library\src\lib\uWURFL.pas',
+  wurfl in '..\..\library\src\lib\wurfl.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfmMain, fmMain);
+  Application.Run;
+end.
